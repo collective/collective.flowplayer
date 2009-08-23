05957fe0e08e27c71c8121de164bd28026922ff1
naro <naro@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-08-23 17:55:09 +0000

    Bah, there are import_steps in another file already :)

--- src/collective/flowplayer/exportimport.py
+++ src/collective/flowplayer/exportimport.py
@@ -1,4 +1,19 @@
 from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.interfaces import IPropertiesTool
+
+# Properties are defined here, because if they are defined in propertiestool.xml,
+# all properties are re-set the their initial state if you reinstall product
+# in the quickinstaller.
+
+_PROPERTIES = [
+    dict(name='player', type_='string', value='${portal_url}/++resource++collective.flowplayer/flowplayer-3.1.2.swf'),
+    dict(name='plugins/controls/url', type_='string', value='${portal_url}/++resource++collective.flowplayer/flowplayer.controls-3.1.2.swf'),
+    dict(name='plugins/audio/url', type_='string', value='${portal_url}/++resource++collective.flowplayer/flowplayer.audio-3.1.0.swf'),
+    dict(name='clip/autoPlay', type_='boolean', value=False),
+    dict(name='clip/autoBuffering', type_='boolean', value=False),
+    dict(name='clip/scaling', type_='string', value='fit'),
+    dict(name='plugins/controls/volume', type_='boolean', value=True),
+]
 
 def import_various(context):
     
@@ -26,3 +41,12 @@ def import_various(context):
     if to_add:
         paragraph_styles += ['%s|%s' % (v, k) for k,v in new_styles if k in to_add]
         kupu.configure_kupu(parastyles=paragraph_styles)
+        
+    # Define portal properties
+    ptool = getToolByName(site, 'portal_properties')
+    props = ptool.flowplayer_properties
+
+    for prop in _PROPERTIES:
+        if not props.hasProperty(prop['name']):
+            props.manage_addProperty(prop['name'], prop['value'], prop['type_'])
+    
