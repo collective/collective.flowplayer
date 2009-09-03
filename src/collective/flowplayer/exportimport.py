c891ccd54effe70778af69613a93911bada74ef6
naro <naro@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-09-03 11:56:08 +0000

    Renamed all files to not contain version numbers. It would be pain to update to the new version.

--- src/collective/flowplayer/exportimport.py
+++ src/collective/flowplayer/exportimport.py
@@ -6,12 +6,12 @@ from Products.CMFPlone.interfaces import IPropertiesTool
 # in the quickinstaller.
 
 _PROPERTIES = [
-    dict(name='player', type_='string', value='${portal_url}/++resource++collective.flowplayer/flowplayer-3.1.2.swf'),
+    dict(name='player', type_='string', value='${portal_url}/++resource++collective.flowplayer/flowplayer.swf'),
     dict(name='loop', type_='boolean', value=False),
     dict(name='showPlaylist', type_='boolean', value=True),
     dict(name='initialVolumePercentage', type_='int', value=50),
-    dict(name='plugins/controls/url', type_='string', value='${portal_url}/++resource++collective.flowplayer/flowplayer.controls-3.1.2.swf'),
-    dict(name='plugins/audio/url', type_='string', value='${portal_url}/++resource++collective.flowplayer/flowplayer.audio-3.1.0.swf'),
+    dict(name='plugins/controls/url', type_='string', value='${portal_url}/++resource++collective.flowplayer/flowplayer.controls.swf'),
+    dict(name='plugins/audio/url', type_='string', value='${portal_url}/++resource++collective.flowplayer/flowplayer.audio.swf'),
     dict(name='clip/autoPlay', type_='boolean', value=False),
     dict(name='clip/autoBuffering', type_='boolean', value=False),
     dict(name='clip/scaling', type_='string', value='fit'),
