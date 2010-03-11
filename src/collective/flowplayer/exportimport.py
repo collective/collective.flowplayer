ee55bea3e178e51b4b9f817d958eae6d3a3fc4ed
naro <naro@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-11 14:11:47 +0000

    Do not fail if Kupu is not available

--- src/collective/flowplayer/exportimport.py
+++ src/collective/flowplayer/exportimport.py
@@ -24,26 +24,27 @@ def import_various(context):
         return
         
     site = context.getSite()
-    kupu = getToolByName(site, 'kupu_library_tool')
+    kupu = getToolByName(site, 'kupu_library_tool', None)
     
-    paragraph_styles = list(kupu.getParagraphStyles())
+    if kupu is not None:
+        paragraph_styles = list(kupu.getParagraphStyles())
     
-    new_styles = [('autoFlowPlayer video', 'Video|div'),
-                  ('autoFlowPlayer video image-left', 'Video (left)|div'),
-                  ('autoFlowPlayer video image-right', 'Video (right)|div'),
-                  ('autoFlowPlayer audio', 'Audio|div'),
-                  ('autoFlowPlayer audio image-left', 'Audio (left)|div'),
-                  ('autoFlowPlayer audio image-right', 'Audio (right)|div')]
-    to_add = dict(new_styles)
+        new_styles = [('autoFlowPlayer video', 'Video|div'),
+                      ('autoFlowPlayer video image-left', 'Video (left)|div'),
+                      ('autoFlowPlayer video image-right', 'Video (right)|div'),
+                      ('autoFlowPlayer audio', 'Audio|div'),
+                      ('autoFlowPlayer audio image-left', 'Audio (left)|div'),
+                      ('autoFlowPlayer audio image-right', 'Audio (right)|div')]
+        to_add = dict(new_styles)
     
-    for style in paragraph_styles:
-        css_class = style.split('|')[-1]
-        if css_class in to_add:
-            del to_add[css_class]
+        for style in paragraph_styles:
+            css_class = style.split('|')[-1]
+            if css_class in to_add:
+                del to_add[css_class]
 
-    if to_add:
-        paragraph_styles += ['%s|%s' % (v, k) for k,v in new_styles if k in to_add]
-        kupu.configure_kupu(parastyles=paragraph_styles)
+        if to_add:
+            paragraph_styles += ['%s|%s' % (v, k) for k,v in new_styles if k in to_add]
+            kupu.configure_kupu(parastyles=paragraph_styles)
         
     # Define portal properties
     ptool = getToolByName(site, 'portal_properties')
