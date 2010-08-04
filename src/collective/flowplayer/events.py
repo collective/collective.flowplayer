842de9b54d2077e53535d70c2e9ab80dc6050932
thet <thet@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-08-04 16:20:30 +0000

    add file_handle property in concrete classes, add attributes in abstract base class

--- src/collective/flowplayer/events.py
+++ src/collective/flowplayer/events.py
@@ -34,10 +34,8 @@ def remove_marker(object):
 class ChangeView(object):
 
     interface = None
-
-    @property.Lazy
-    def value(self):
-        return self.content.getField('file').getRaw(self.content)
+    value = None
+    file_handle = None
 
     def handleAudio(self):
         if not IAudio.providedBy(self.content):
@@ -81,6 +79,19 @@ class ChangeFileView(ChangeView):
 
     interface = interface.IFileContent
 
+    @property.Lazy
+    def value(self):
+        return self.content.getField('file').getRaw(self.content)
+
+    @property.Lazy
+    def file_handle(self):
+        file_object = self.value
+        try:
+            # For blobs
+            file_handle = file_object.getIterator()
+        except AttributeError:
+            file_handle = StringIO(str(file_object.data))
+
     def check_extension(self):
         filename = self.value.filename
         if isinstance(filename, basestring):
@@ -91,14 +102,7 @@ class ChangeFileView(ChangeView):
         return None
 
     def handleVideo(self):
-        file_object = self.value
-        try:
-            # For blobs
-            file_handle = file_object.getIterator()
-        except AttributeError:
-            file_handle = StringIO(str(file_object.data))
-
-        metadata = extract_from_raw(file_handle)
+        metadata = extract_from_raw(self.file_handle)
         height, width = scale_from_metadata(metadata)
 
         super(ChangeFileView, self).handleVideo()
@@ -116,6 +120,14 @@ class ChangeLinkView(ChangeView):
     def value(self):
         return self.content.getField('remoteUrl').getRaw(self.content)
 
+    @property.Lazy
+    def file_handle(self):
+        try:
+            file_handle = urllib2.urlopen(self.content.getRemoteUrl())
+        except IOError:
+            file_handle = StringIO()
+        return file_handle
+
     def check_extension(self):
         filename = self.value.lower()
         for ext in AUDIO_EXTENSIONS + VIDEO_EXTENSIONS:
@@ -124,12 +136,7 @@ class ChangeLinkView(ChangeView):
         return None
 
     def handleVideo(self):
-        try:
-            file_handle = urllib2.urlopen(self.content.getRemoteUrl())
-        except IOError:
-            file_handle = StringIO()
-
-        metadata = extract_from_raw(file_handle)
+        metadata = extract_from_raw(self.file_handle)
         height, width = scale_from_metadata(metadata)
 
         super(ChangeLinkView, self).handleVideo()
