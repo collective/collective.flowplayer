4bee6e6978f5cae7469af2d118904394a4b0340e
thet <thet@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-08-04 16:18:53 +0000

    fix testlayer setup

--- src/collective/flowplayer/testing.py
+++ src/collective/flowplayer/testing.py
@@ -1,6 +1,6 @@
 from Products.PloneTestCase import ptc
-
 from collective.testcaselayer import ptc as tcl_ptc
+from Products.Five import zcml
 
 ptc.setupPloneSite()
 
@@ -8,6 +8,8 @@ class Layer(tcl_ptc.BasePTCLayer):
     """Install collective.flowplayer"""
 
     def afterSetUp(self):
+        import collective.flowplayer
+        zcml.load_config('configure.zcml', collective.flowplayer)
         self.addProfile('collective.flowplayer:default')
         # put resource registry to debug mode to avoid cachekyes in tests
         self.portal.portal_css.setDebugMode(True)
