6ff8108256aef788f3e69706b0edb1bb9b11dfb9
ldr <ldr@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-03-16 17:17:34 +0000

    Only generate a minimal config global object. Move remaining javascript to a   separate file in a resource directory to simplify checking with jslint and   customisation.

--- src/collective/flowplayer/browser/view.py
+++ src/collective/flowplayer/browser/view.py
@@ -46,111 +46,21 @@ class JavaScript(BrowserView):
         return flash_properties_to_dict(self.flowplayer_properties,
                                         portal_url)
 
-    def update(self):
-        portal_url = self.portal_state().portal_url()
-        if not portal_url.endswith('/'):
-            portal_url += '/'
-
-        # if showPlaylist is True, do not show playlist buttons on controlbar
-        self.show_cb_playlist_buttons = not self.flowplayer_properties.getProperty('showPlaylist')
-        # build string in Javascript format which is appended to the player
-        # It contains javascript events which can't be configured in the
-        # self.properties, because simplejson can't handle them
-        self.events = ''
-        volume = self.flowplayer_properties.getProperty('initialVolumePercentage')
-        if volume:
-            self.events += '.onLoad( function() { this.setVolume(%d); })' % volume
-        if self.flowplayer_properties.getProperty('loop'):
-            self.events += '.onBeforeFinish( function() { return false; })'
-
-
     def __call__(self, request=None, response=None):
         """ Returns global configuration of the Flowplayer taken from portal_properties """
-        self.update()
         self.request.response.setHeader("Content-type", "text/javascript")
+        data = dict(
+            config=self.flowplayer_properties_as_dict,
+            params=self.flash_properties_as_dict,
+            loop=bool(self.flowplayer_properties.getProperty('loop')),
+            )
+        volume = self.flowplayer_properties.getProperty('initialVolumePercentage')
+        if volume:
+            data['initialVolumePercentage'] = int(volume)
+        else:
+            data['initialVolumePercentage'] = None
+        return "var collective_flowplayer = %s;" % simplejson.dumps(data, indent=2)
 
-        return """(function($) {
-        $(function() {
-            $('.autoFlowPlayer').each(function() {
-                var config = %(config)s;
-                var $self = $(this);
-                if ($self.is('.minimal')) {
-                    config.plugins.controls = null;
-                };
-                var audio = $self.is('.audio');
-                if (audio && !$self.is('.minimal')) {
-                    config.plugins.controls.all = false;
-                    config.plugins.controls.play = true;
-                    config.plugins.controls.scrubber = true;
-                    config.plugins.controls.mute = true;
-                    config.plugins.controls.volume = true;
-                    config.plugins.controls.time = true;
-                    config.plugins.controls.autoHide = false;
-                }
-                if ($self.is('div')) {
-                    // comming from Kupu, there are relative urls
-                    config.clip.baseUrl = $('base').attr('href');
-                    config.clip.url = $self.find('a').attr('href');
-                    if (audio) {
-                      // force .mp3 extension
-                      config.clip.url = config.clip.url + '?e=.mp3';
-                    };
-                    // Ignore global autoplay settings
-                    if ($self.find('img').length == 0) {
-                        // no image. Don't autoplay, remove all elements inside the div to show player directly.
-                        config.clip.autoPlay = false;
-                        $self.empty();
-                    } else {
-                        // Clip is probably linked as image, so autoplay the clip after image is clicked
-                        config.clip.autoPlay = true;
-                    }
-                }
-                flowplayer(this, %(params)s, config)%(events)s;
-                $('.flowPlayerMessage').remove();
-            });
-            $('.playListFlowPlayer').each(function() {
-                var config = %(config)s;
-                var $self = $(this);
-                var audio = $self.is('.audio');
-                if (audio) { config.plugins.controls.fullscreen = false; }
-                if ($self.is('.minimal')) { config.plugins.controls = null; }
-                if ($self.find('img').length > 0) {
-                    // has splash
-                    config.clip.autoPlay = true;
-                }
-                portlet_parents = $self.parents('.portlet');
-                var playlist_selector = 'div#flowPlaylist';
-                if (portlet_parents.length > 0) {
-                    var portlet = true;
-                    // playlist has to be bound to unique item
-                    playlist_selector_id = portlet_parents.parent().attr('id')+'-playlist';
-                    $self.parent().find('.flowPlaylist-portlet-marker').attr('id', playlist_selector_id);
-                    playlist_selector = '#'+playlist_selector_id;
-                    if (audio && !$self.is('.minimal')) {
-                        config.plugins.controls.all = false;
-                        config.plugins.controls.play = true;
-                        config.plugins.controls.scrubber = true;
-                        config.plugins.controls.mute = true;
-                        config.plugins.controls.volume = false;
-                    }
-                } else {
-                    var portlet = false;
-                }
-                if (!portlet) {
-                    $("#pl").scrollable({items:playlist_selector, size:4, clickable:false, prev:'a.prevPage', next:'a.nextPage'});
-                }
-                // manual = playlist is setup using HTML tags, not using playlist array in config
-                flowplayer(this, %(params)s, config).playlist(playlist_selector, {loop: true, manual: true})%(events)s;
-                $self.show();
-                $('.flowPlayerMessage').remove();
-
-            });
-        });
-})(jQuery);
-""" % dict(params = simplejson.dumps(self.flash_properties_as_dict),
-           config = simplejson.dumps(self.flowplayer_properties_as_dict, indent=4),
-           events = self.events,
-          )
 
 class File(BrowserView):
     interface.implements(IFlowPlayerView)
