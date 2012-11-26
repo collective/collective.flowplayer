bbbaf35cd7625eb662a526c2b369241f3a99b8fe
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-11-26 13:55:15 +0100

    replace tabs with spaces

--- src/collective/flowplayer/browser/js/init.js
+++ src/collective/flowplayer/browser/js/init.js
@@ -47,7 +47,7 @@ initFlowpalyer = function(area){
             } else {
                 // Clip is probably linked as image, so autoplay the clip after image is clicked
                 config.clip.autoPlay = true;
-				// If we know there is splash image, we could at least try to match this size
+                // If we know there is splash image, we could at least try to match this size
                 $(this).css("width", $self.find('img').width());
                 $(this).css("height", $self.find('img').height());
             }
